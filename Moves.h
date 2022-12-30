85472eb4d06094ff585438b8c1df120bfd4e19a7
AhmedWaleed9 <95943296+AhmedWaleed9@users.noreply.github.com>
2022-12-31 00:31:12 +0200

    Update Moves.h

--- Moves.h
+++ Moves.h
@@ -2,6 +2,7 @@
 // Created by Ahmed Waleed on 12/30/2022.
 //
 #include <bits/stdc++.h>
+
 using namespace std;
 
 #ifndef MAIN_CPP_MOVES_H
@@ -10,10 +11,17 @@ using namespace std;
 
 class Moves {
     int ctr;
+    int limit;
     stack<string> moves;
 public:
     Moves();
+
+    // Setters
+    void setLimit(int l);
+
+    // Methods
     void addMove(string s);
+
     string undoMove();
 };
 
